17ee63360177eb55fa5236147bb6b3fc403add96
prafull6675 <140291363+vidya6675@users.noreply.github.com>
2024-02-14 15:36:49 +0530

    Update CompoundOp3.java

--- CompoundOp3.java
+++ CompoundOp3.java
@@ -8,7 +8,10 @@ age=age*age1;
 System.out.println(age);//compile time error
 */
 age*=age1;
-System.out.println(age);//12
+System.out.println(age);//25
 
 }
 }
+o/p=Compiletime success
+  Runtime success
+  o/p=25
