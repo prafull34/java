4de2f78203a97da72f32228e0a87b9d970fa3258
prafull6675 <140291363+vidya6675@users.noreply.github.com>
2024-02-14 15:34:03 +0530

    Update CompoundOp2 (2).java

--- CompoundOp2 (2).java
+++ CompoundOp2 (2).java
@@ -8,7 +8,10 @@ age=age-age1;
 System.out.println(age);//compile time error
 */
 age-=age1;
-System.out.println(age);//12
+System.out.println(age);//7
 
 }
 }
+o/p=Compiletime success
+  Runtime suucess
+  o/p=7;
