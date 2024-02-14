d3a9549b50106668dbc1aa044ee064a4c172b2b1
prafull6675 <140291363+vidya6675@users.noreply.github.com>
2024-02-14 15:20:33 +0530

    Add files via upload

---
+++ LastDigitOfNumber.java
@@ -0,0 +1,9 @@
+class LastDigitOfNumber
+{
+public static void main(String[]ldon)
+{
+int number=1258647;
+int last_Digit=number%10;
+System.out.println(number%10);
+}
+}
---
+++ printintvalue.java
@@ -0,0 +1,8 @@
+class PrintIntValue
+{
+public static void main(String[] args)
+{
+int value1=10;
+System.out.println(value1);
+}
+}
