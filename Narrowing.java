3a59b5c43fd7fbfb03a08e6f4de4e11d548ed31a
prafull6675 <140291363+vidya6675@users.noreply.github.com>
2024-02-22 03:16:33 +0530

    Add files via upload

---
+++ Narrowing.java
@@ -0,0 +1,13 @@
+public class Narrowing
+{
+public static void main(String[]Na)
+{
+float temp=1526;
+byte City_Temp=(byte)temp;
+System.out.println(City_Temp);
+}
+}
+   /*output
+     compiletime=success
+     runtime=sucess
+     o/p-10 */
