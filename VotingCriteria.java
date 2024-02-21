8cf1efc04a9b7c244e11a14c93f94b9edc13a9f8
prafull6675 <140291363+vidya6675@users.noreply.github.com>
2024-02-22 02:30:56 +0530

    Add files via upload

---
+++ VotingCriteria.java
@@ -0,0 +1,11 @@
+public class VotingCriteria
+{
+public static void main(String[]piv)
+{
+int rahulage=1;
+//Voting Criteria age>=18
+String Result=rahulage>=18 ?("Congrates you can vote"):("Sorry you are not eligible");
+//21>=18;
+System.out.println(Result);
+}
+}
