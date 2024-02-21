3e505f824db42bb26e6983bc02dabc7f5bb0b31c
prafull6675 <140291363+vidya6675@users.noreply.github.com>
2024-02-22 03:04:55 +0530

    Add files via upload

---
+++ WideningExplicitTypeCast.java
@@ -0,0 +1,12 @@
+public class WideningExplicitTypeCast
+{
+public static void main(String[]wtv)
+{
+int a=10;    //Explicit typecast
+float b=a;
+System.out.println(b);
+int a1=10;
+float b1=(int)a1;
+System.out.println(b1);
+}
+}
---
+++ WideningImplicitTypeCast.java
@@ -0,0 +1,9 @@
+public class WideningImplicitTypeCast
+{
+public static void main(String[]wtv)
+{
+int a=10;
+float b=a;
+System.out.println(b);
+}
+}
