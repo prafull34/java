94b8ad0e092cbd0a4e34dac6ef8eacfb63b57b2d
prafull6675 <140291363+vidya6675@users.noreply.github.com>
2024-02-24 06:16:23 +0530

    Add files via upload

---
+++ BuzzChecl.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class BuzzChecl{
+   public static void main(String []args){
+    Scanner sc=new Scanner(System.in);
+    int a=sc.nextInt();
+    String ans=(a>99 ||a<9)?("Invalid Number"):(a%10==7|| a%77==0?("Buzz Number"):("Not Buzz Number"));
+    System.out.println(ans);
+    sc.close();
+  }
+}
